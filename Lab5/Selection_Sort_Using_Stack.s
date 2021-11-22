2c2478cbc33bcfff028329903cc4c5cd783d3917
Sarthak Shastri <55555570+sarthak815@users.noreply.github.com>
2021-11-23 02:25:10 +0530

    Lab5-6

--- Lab5/Selection_Sort_Using_Stack.s
+++ Lab5/Selection_Sort_Using_Stack.s
@@ -13,37 +13,12 @@ __Vectors
 
 Reset_Handler
 		
-		LDR R0,=NUM
-		LDM R0,{R1-R10}
-		STMIA r13!, {r1-r10}
-		MOV r0, r13 ;r0 stores the stack top
-		MOV r2, #10 ;r2 stores number of elements in stack
-		MOV r8,#0	;r8 is counter for outer loop
-ol		
-		CMP r8,r2
-		BEQ exit
-		
-		MOV r1, r0	
-		MOV r3, r0
-		SUB r3, #4
-		ADD r9,r8,#1
-il		
-		CMP r9,r2
-		BEQ exin
-		ADD r9,#1
-		LDMDB r1,{r4}
-		LDMDB r3!,{r5}
-		CMP r5,r4
-		BLT il
-		STMDB r1,{r5}
-		STM r3,{r4}
-		B il
-exin	SUB r0,#4
-		ADD r8,#1
-		B ol
-		
-exit	
-		LDMDB r13!,{r1-r10}
+		MOV R0,#10
+		MOV R1,#10
+		mov R2,#2
+		MOV R3,#10
+		SMLAL R0,R2,R1,R3
+	
 STOP 	
 	B STOP
 NUM DCD 1,6,4,5,9,3,2,7,8,10
